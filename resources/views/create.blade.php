b8a7836efed957fc3e3c7a1c1d84d8671ae2a1fe
Ashura <the.lucas2016@gmail.com>
2019-08-16 22:41:10 -0300

    Mostrar errors no form

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,6 +9,12 @@
         <div class="form-group">
             <label for="name">Nome</label>
             <input type="text" class="form-control" id="name" value="{{ old('name') }}">
+
+            @if($errors->has('name'))
+                <div class="invalid-feedback">
+                    {{ $errors->first('name') }}
+                </div>
+            @endif 
         </div>
 
         <div class="form-group">
@@ -16,16 +22,34 @@
             <textarea type="text" class="form-control" id="description">
                 {{ old('description') }}
             </textarea>
+
+            @if($errors->has('description'))
+                <div class="invalid-feedback">
+                    {{ $errors->first('description') }}
+                </div>
+            @endif 
         </div>
 
         <div class="form-group">
             <label for="price">Preço</label>
             <input type="text" class="form-control" id="price" value="{{ old('price') }}">
+
+            @if($errors->has('price'))
+                <div class="invalid-feedback">
+                    {{ $errors->first('price') }}
+                </div>
+            @endif 
         </div>
 
         <div class="form-group">
             <label for="vendor">Fornecedor</label>
             <input type="text" class="form-control" id="vendor" value="{{ old('vendor') }}">
+
+            @if($errors->has('vendor'))
+                <div class="invalid-feedback">
+                    {{ $errors->first('vendor') }}
+                </div>
+            @endif 
         </div>
 
         <button type="submit" class="btn btn-primary mt-2">

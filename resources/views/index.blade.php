84b0b9b46bc19e640563339fcb70bba868c7f909
Ashura <the.lucas2016@gmail.com>
2019-08-16 21:47:23 -0300

    Colocar um header padrão no layout e criar uma seção para adicionar um título na página

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,84 +1,56 @@
 @extends('layout.app')
 
+@section('title', 'Lista de produtos')
+
 @section('content')
-    <div class="jumbotron">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-3">
-                    <a href="{{ url('/') }}" class="h2 text-decoration-none text-dark">
-                        Lista de produtos
-                    </a>
-                </div>
+    <!-- Select products count -->
+    <div class="my-4">
+        <span class="d-inline-block">Mostrando</span>
 
-                <div class="col-md-5">
-                    <form>
-                        <div class="input-group">
-                            <input type="text" name="query" class="form-control" placeholder="Buscar produtos baseado no nome, descrição, fornecedor, preço...">
-                            <input type="hidden" name="productsPerPage" value="{{ $productsPerPage }}">
+        <select id="select-products-count" class="form-control col-1 mx-1 d-inline-block cursor-pointer">
+            @for ($productsCount = 5; $productsCount <= 50; $productsCount += 5)
+                <option {{ $productsCount == $productsPerPage ? 'selected' : null }} value="{{ $productsCount }}">
+                    {{ $productsCount }}
+                </option>
+            @endfor
+        </select>
 
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="submit">
-                                    <i class="fas fa-search"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-
-                <div class="col-md-2 ml-auto">
-                    <a href="{{ url('/products/create') }}" class="btn btn-primary">
-                        Adicionar
-                    </a>
-                </div>
-            </div>
-
-            <div class="my-4">
-                <span class="d-inline-block">Mostrando</span>
-
-                <select id="select-products-count" class="form-control col-1 mx-1 d-inline-block cursor-pointer">
-                    @for ($productsCount = 5; $productsCount <= 50; $productsCount += 5)
-                        <option {{ $productsCount == $productsPerPage ? 'selected' : null }} value="{{ $productsCount }}">
-                            {{ $productsCount }}
-                        </option>
-                    @endfor
-                </select>
-
-                <span class="d-inline-block">produtos por página.</span>
-            </div>
-
-            <table class="table mt-3">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Nome</th>
-                        <th>Descrição</th>
-                        <th>Preço</th>
-                        <th>Fornecedor</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($products as $product)
-                        <tr>
-                            <td>{{ $product->id }}</td>
-                            <td>{{ $product->name }}</td>
-                            <td>{{ str_limit($product->description, 150, '...') }}</td>
-                            <td>R$ {{ $product->price }}</td>
-                            <td>{{ $product->vendor }}</td>
-                            <td class="btn-group">
-                                <a class="btn btn-warning btn-sm" href="/">
-                                    Editar
-                                </a>
-                                <a class="btn btn-danger btn-sm" href="/">
-                                    Excluir
-                                </a>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-
-            {{ $products->links() }}
-        </div>
+        <span class="d-inline-block">produtos por página.</span>
     </div>
+
+    <!-- Table -->
+    <table class="table">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Descrição</th>
+                <th>Preço</th>
+                <th>Fornecedor</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($products as $product)
+                <tr>
+                    <td>{{ $product->id }}</td>
+                    <td>{{ $product->name }}</td>
+                    <td>{{ str_limit($product->description, 150, '...') }}</td>
+                    <td>R$ {{ $product->price }}</td>
+                    <td>{{ $product->vendor }}</td>
+                    <td class="btn-group">
+                        <a class="btn btn-warning btn-sm" href="/">
+                            Editar
+                        </a>
+                        <a class="btn btn-danger btn-sm" href="/">
+                            Excluir
+                        </a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+
+    <!-- Pagination -->
+    {{ $products->links() }}
 @endsection
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,10 +6,46 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    <title>{{ config('app.name') }}</title>
+    <title>
+        @yield('title') - {{ config('app.name') }}
+    </title>
 </head>
 <body>
-    @yield('content')
+    <div class="jumbotron">
+        <div class="container">
+            <!-- Header -->
+            <header class="row align-items-center mb-3">
+                <div class="col-md-3">
+                    <a href="{{ url('/') }}" class="h2 text-decoration-none text-dark">
+                        Lista de produtos
+                    </a>
+                </div>
+
+                <div class="col-md-5">
+                    <form>
+                        <div class="input-group">
+                            <input type="text" name="query" class="form-control" placeholder="Buscar produtos baseado no nome, descrição, fornecedor, preço...">
+                            <input type="hidden" name="productsPerPage" value="{{ $productsPerPage }}">
+
+                            <div class="input-group-append">
+                                <button class="btn btn-primary" type="submit">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+
+                <div class="col-md-2 ml-auto">
+                    <a href="{{ url('/products/create') }}" class="btn btn-primary">
+                        Adicionar
+                    </a>
+                </div>
+            </header>
+
+            @yield('content')
+        </div>
+    </div>
 
     <script src="https://kit.fontawesome.com/5a3176b5aa.js"></script>
     <script src="{{ asset('/js/app.js') }}"></script>

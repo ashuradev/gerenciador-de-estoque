c55929d3d76c8bd25248f4e4b5828910be479fac
Ashura <the.lucas2016@gmail.com>
2019-08-17 12:20:52 -0300

    Algumas melhorias

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,7 @@
     <div class="my-4">
         <span class="d-inline-block">Mostrando</span>
 
-        <select id="select-products-count" class="form-control col-1 mx-1 d-inline-block cursor-pointer">
+        <select id="select-products-count" class="form-control col-md-1 col-2 mx-1 d-inline-block cursor-pointer">
             @for ($productsCount = 5; $productsCount <= 50; $productsCount += 5)
                 <option {{ $productsCount == $productsPerPage ? 'selected' : null }} value="{{ $productsCount }}">
                     {{ $productsCount }}
@@ -39,7 +39,7 @@
                     <td>R$ {{ $product->price }}</td>
                     <td>{{ $product->vendor }}</td>
                     <td class="btn-group">
-                        <a class="btn btn-warning btn-sm" href="/">
+                        <a class="btn btn-warning btn-sm" href="{{ route('products.edit', [$product]) }}">
                             Editar
                         </a>
                         <a class="btn btn-danger btn-sm" href="/">

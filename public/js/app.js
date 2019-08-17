819e3466b2581e3a7bafceef7350f392540d3491
Ashura <the.lucas2016@gmail.com>
2019-08-16 21:38:16 -0300

    Remover linha

--- public/js/app.js
+++ public/js/app.js
@@ -3,6 +3,5 @@ document
     .onchange = function() {
         const href = location.href.replace(/productsPerPage=.*/, 'productsPerPage=' + this.value)
 
-
         location.href = href.includes('?') ? href : '?productsPerPage=' + this.value
     }
